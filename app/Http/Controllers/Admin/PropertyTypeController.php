6f327348a081f7174ad5be4aca9093195733890f
Neil Patrick Mulingbayan <neilpatrick.personal@gmail.com>
2025-05-29 16:23:33 +0800

    add property type model w/ -mc

---
+++ app/Http/Controllers/Admin/PropertyTypeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PropertyTypeController extends Controller
+{
+    //storing for new added property type 
+    public function store(){
+        
+    }
+}
