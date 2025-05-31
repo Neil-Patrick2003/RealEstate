5873a067fceb34f5314c146498a639c2493f24ba
Neil Patrick Mulingbayan <neilpatrick.personal@gmail.com>
2025-05-31 22:22:59 +0800

    add property_image migration and model

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Property extends Model
 {
     protected  $guarded = [];
+
+    public function image(){
+        return $this->hasMany(PropertyImage::class);
+    }
 }
