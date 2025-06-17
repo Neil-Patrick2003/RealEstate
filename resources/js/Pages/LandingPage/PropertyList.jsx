429bd0d70b2ff22a96db09766f07f3015a7c2967
Neil Patrick Mulingbayan <neilpatrick.personal@gmail.com>
2025-06-18 06:53:55 +0800

    tesstt

--- resources/js/Pages/LandingPage/PropertyList.jsx
+++ resources/js/Pages/LandingPage/PropertyList.jsx
@@ -4,6 +4,8 @@ const PropertyList = () => {
   return (
     <div className='h-screen bg-orange-300 flex justify-center items-center'>
         property list
+
+        helo world
     </div>
   )
 }
