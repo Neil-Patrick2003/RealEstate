bd15e3b36c9777f40d1b28ed38f54eb68cde2d4e
Neil Patrick Mulingbayan <neilpatrick.personal@gmail.com>
2025-06-18 06:47:24 +0800

    test

--- resources/js/Pages/LandingPage/PropertyList.jsx
+++ resources/js/Pages/LandingPage/PropertyList.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const PropertyList = () => {
   return (
     <div className='h-screen bg-orange-300 flex justify-center items-center'>
-        property list hahah
+        property list hahahhahahah
     </div>
   )
 }
