497ce374d91b91013efe88c2d779abdb9df9ca05
Neil Patrick Mulingbayan <neilpatrick.personal@gmail.com>
2025-06-18 08:10:05 +0800

    fix

--- resources/js/Pages/Seller/Properties/EditProperty.jsx
+++ resources/js/Pages/Seller/Properties/EditProperty.jsx
@@ -1,10 +1,13 @@
+import Collapsable from '@/Components/collapsable/collapsable'
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout'
 import React from 'react'
 
 const EditProperty = ({property}) => {
   return (
     <AuthenticatedLayout>
+        <Collapsable></Collapsable>
         {property.title}
+
     </AuthenticatedLayout>
   )
 }
