091d5a97d8d0451e183e656d255b1e887d667804
Bryan Mulingbayan <bryan.mulingbayan96@gmail.com>
2025-07-13 17:07:59 +0800

    Fix

--- resources/js/Components/Chat/ChannelItem.jsx
+++ resources/js/Components/Chat/ChannelItem.jsx
@@ -1,8 +1,9 @@
-import {usePage} from '@inertiajs/react';
+import {usePage, Link} from '@inertiajs/react';
 import {useMemo} from 'react';
 
 const ChannelItem = ({ channel, isActive = false}) => {
     const {user} = usePage().props.auth
+    const { url } = usePage();
 
     const title = useMemo(() => {
         return channel.members.filter(member => member.id !== user.id)
@@ -10,9 +11,12 @@ const ChannelItem = ({ channel, isActive = false}) => {
             .join(',');
     }, [user, channel.members]);
 
-    return <div className={`px-2 py-4 cursor-pointer ${isActive ? 'bg-green-100': 'bg-transparent'}`}>
-        {title}
-    </div>
+    return <Link
+            className={`px-2 py-4 cursor-pointer w-full flex ${isActive ? 'bg-green-100': 'bg-transparent'}`}
+            href={`${url.includes('agents') ? '/agents': '/seller'}/chat/channels/${channel.id}`}
+        >
+            {title}
+        </Link>
 }
 
 export default ChannelItem
