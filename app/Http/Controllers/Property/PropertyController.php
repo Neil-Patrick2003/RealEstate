4a98879180e2c5bce02359ce33c52505741c4ad9
Neil Patrick Mulingbayan <neilpatrick.personal@gmail.com>
2025-07-18 23:23:15 +0800

    properties in map

---
+++ app/Http/Controllers/Property/PropertyController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Property;
+
+use App\Http\Controllers\Controller;
+
+class PropertyController extends Controller
+{
+    public function index()
+    {
+
+    }
+}
