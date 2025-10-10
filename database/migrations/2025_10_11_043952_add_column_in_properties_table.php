ab0fa74ab49ec0241074b4e48a69fb64ebaa59f7
Neil Patrick Mulingbayan <neilpatrick.personal@gmail.com>
2025-10-11 05:00:23 +0800

    Added isFixPrice and reservation fee in properties table

---
+++ database/migrations/2025_10_11_043952_add_column_in_properties_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('properties', function (Blueprint $table) {
+            $table->boolean('isFixPrice')->default(false)->after('price');
+            $table->decimal('reservation', 8, 2)->nullable()->after('price');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('properties', function (Blueprint $table) {
+            $table->dropColumn('isFixPrice');
+            $table->dropColumn('reservation');
+        });
+    }
+};
