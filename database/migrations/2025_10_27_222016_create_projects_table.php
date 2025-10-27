ead3808e1082c4f0fc1119a47e0a3008403864f6
Neil Patrick Mulingbayan <neilpatrick.personal@gmail.com>
2025-10-27 22:25:05 +0800

    Added Project model and migration

---
+++ database/migrations/2025_10_27_222016_create_projects_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('developer_id');
+            $table->string('name');
+            $table->string('type');
+            $table->string('address');
+            $table->string('status');
+            $table->foreign('developer_id')->references('id')->on('developers')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
